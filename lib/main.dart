a09a468f17362bdf07f29490aee2574415f50bff
ersaoktavian <ersaoktavianramadan@gmail.com>
2024-11-13 09:06:32 +0700

    W12: Soal 17

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 import 'package:async/async.dart';
 import 'geolocation.dart';
-import 'navigation_first.dart'; // Import NavigationFirst
+import 'navigation_first.dart';
+import 'navigation_dialog.dart'; // Import NavigationDialogScreen
 
 void main() {
   runApp(const MyApp());
@@ -21,77 +22,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const NavigationFirst(), // Ganti dengan NavigationFirst
-    );
-  }
-}
-
-class FuturePage extends StatefulWidget {
-  const FuturePage({super.key});
-
-  @override
-  State<FuturePage> createState() => _FuturePageState();
-}
-
-class _FuturePageState extends State<FuturePage> {
-  String result = '';
-
-  Future<int> returnOneAsync() async {
-    await Future.delayed(const Duration(seconds: 3));
-    return 1;
-  }
-
-  Future<int> returnTwoAsync() async {
-    await Future.delayed(const Duration(seconds: 3));
-    return 2;
-  }
-
-  Future<int> returnThreeAsync() async {
-    await Future.delayed(const Duration(seconds: 3));
-    return 3;
-  }
-
-  Future returnError() async {
-    await Future.delayed(const Duration(seconds: 2));
-    throw Exception('Something terrible happened!');
-  }
-
-  Future handleError() async {
-    try {
-      await returnError();
-    } catch (error) {
-      setState(() {
-        result = error.toString();
-      });
-    } finally {
-      print('Complete');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Back From The Future - Ersa'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            const Spacer(),
-            ElevatedButton(
-              child: const Text('GO!'),
-              onPressed: () {
-                handleError();
-              },
-            ),
-            const Spacer(),
-            Text(result),
-            const Spacer(),
-            const CircularProgressIndicator(),
-            const Spacer(),
-          ],
-        ),
-      ),
+      home: const NavigationDialogScreen(), // Ubah ke NavigationDialogScreen
     );
   }
 }
